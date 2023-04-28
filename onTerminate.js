697a1280406ecf31395f3ac3aa88fe58861e4ece
Maurizio Lupo <maurizio.lupo@gmail.com>
2023-04-28 21:50:56 +0100

    updated deps

--- onTerminate.js
+++ onTerminate.js
@@ -1,25 +1,20 @@
-const defaults = {
+const DEFAULTS = {
   exitDelay: 50,
   stopWindow: 5000,
   logger: (message, err) => console.log(message, err)
 }
-
-const isNil = (x) => typeof x === 'undefined' || x === null
 
 const timeoutMessage = (tm, message) => new Promise((resolve) => {
   setTimeout(() => resolve(message), tm).unref()
 })
 
 function onTerminate (shutdownFunction, options) {
-  options = Object.assign({}, defaults, options)
-  const logger = options.logger
-  const customEvent = options.customEvent
-  const handleExceptions = options.handleExceptions
+  const {logger, customEvent, handleExceptions, stopWindow, exitDelay} = {...DEFAULTS, ...options}
 
   function shutDown (reason, code) {
     logger(`Received ${reason}. Attempting to shutdown gracefully.`)
 
-    if (!isNil(code)) {
+    if (code != null) {
       process.exitCode = code
     }
 
@@ -33,9 +28,9 @@ function onTerminate (shutdownFunction, options) {
       process.removeListener('unhandledRejection', unhandledRejectionListener)
     }
 
-    const timeout = timeoutMessage(options.stopWindow, `Graceful shutdown took more than stop window (${options.stopWindow} ms). Terminating process.`)
+    const timeoutFunction = timeoutMessage(stopWindow, `Graceful shutdown took more than stop window (${stopWindow} ms). Terminating process.`)
 
-    return Promise.race([shutdownFunction(reason), timeout])
+    return Promise.race([shutdownFunction(reason), timeoutFunction])
       .then((message) => {
         if (typeof message === 'string') {
           logger(message)
@@ -49,10 +44,13 @@ function onTerminate (shutdownFunction, options) {
   }
 
   function exitSoon (code) {
-    setTimeout(() => process.exit(code), options.exitDelay).unref()
+    setTimeout(() => process.exit(code), exitDelay).unref()
   }
 
   const stopListener = (payload) => shutDown(`'${customEvent}'`, payload && payload.code)
+  const sigintListener = () => shutDown('SIGINT')
+  const sigtermListener = () => shutDown('SIGINT')
+
   const uncaughtExceptionListener = (err) => {
     console.error(err)
     shutDown('uncaughtException', 1)
@@ -61,8 +59,6 @@ function onTerminate (shutdownFunction, options) {
     console.error(err)
     shutDown('unhandledRejection', 1)
   }
-  const sigintListener = shutDown.bind(null, 'SIGINT', undefined)
-  const sigtermListener = shutDown.bind(null, 'SIGTERM', undefined)
 
   if (customEvent) {
     process.once(customEvent, stopListener)
